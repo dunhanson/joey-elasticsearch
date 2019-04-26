aec849f56d0c12263995ab89e0e63ec92b44eec0
dunhanson <dunhanson@gmail.com>
2019-04-26 10:39:08 +0800

    删除空格

--- src/main/java/cn/joey/elasticsearch/utils/ElasticsearchUtils.java
+++ src/main/java/cn/joey/elasticsearch/utils/ElasticsearchUtils.java
@@ -9,7 +9,6 @@ import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
